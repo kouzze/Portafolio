6d80df486b07bfa3655d3a40549b7dee9af38b78
Nicolas Jara Carvajal <nicolas.jaracarvajal@utem.cl>
2025-02-16 21:48:28 -0300

    Add google tag manager

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Tag Manager */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-X6H7HG68GD"
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-X6H7HG68GD');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
